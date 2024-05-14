c4b9006966a2954bb74d7947f87e4f85b79d052a
Joseph Maria <joseph.maria@seguente.tech>
2024-05-14 15:20:22 -0400

    added supertest testing and better jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,11 @@
 module.exports = {
     preset: "ts-jest",
     testEnvironment: "node",
-    moduleDirectories: ["node_modules", "src"],
+    moduleDirectories: ["node_modules"],
+    clearMocks: true,
+    collectCoverageFrom: ["<rootDir>/src/**/*.ts"],
+    moduleNameMapper: {
+        // eslint-disable-next-line @typescript-eslint/naming-convention
+        "@/(.*)": "<rootDir>/src/$1",
+    },
 };
