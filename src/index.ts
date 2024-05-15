633141b0d393729feae3421cf1c8fffd2b8b8346
Joseph Maria <joseph.maria@seguente.tech>
2024-05-15 15:29:43 -0400

    added fatal error handling for server

--- src/index.ts
+++ src/index.ts
@@ -6,8 +6,31 @@ const server = app.listen(config.PORT, () => {
     logger.info(`Listening to port: ${config.PORT}`);
 });
 
+const exitHandler = () => {
+    if (server) {
+        server.close(() => {
+            logger.info("Server closed");
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+};
+
+const unexpectedErrorHandler = (error: Error) => {
+    logger.fatal("unexpectedErrorHandler", error);
+    exitHandler();
+};
+
+process.on("uncaughtException", unexpectedErrorHandler);
+process.on("unhandledRejection", unexpectedErrorHandler);
+
 process.on("SIGTERM", () => {
-    server.close(() => {
-        logger.fatal("HTTP server closed");
-    });
+    logger.fatal("SIGTERM received");
+    exitHandler();
+});
+
+process.on("SIGINT", () => {
+    logger.fatal("SIGINT received");
+    exitHandler();
 });
