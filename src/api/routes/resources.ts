44934c3d36c1fc95152bf1363a22706e278b1303
morch20 <82046309+morch20@users.noreply.github.com>
2024-06-04 11:48:32 -0400

    validation update

--- src/api/routes/resources.ts
+++ src/api/routes/resources.ts
@@ -6,14 +6,14 @@ const resourceRoute = Router();
 
 resourceRoute
     .route("/")
-    .post(validate(resourceSchema), resourceController.createResource)
+    .post(validate(resourceSchema, "body"), resourceController.createResource)
     .get(resourceController.getResources);
 
 // eslint-disable-next-line drizzle/enforce-delete-with-where
 resourceRoute
-    .route("/:userId")
+    .route("/:id")
     .get(resourceController.getResource)
-    .patch(validate(resourceSchema), resourceController.updateResource)
+    .patch(validate(resourceSchema, "body"), resourceController.updateResource)
     .delete(resourceController.deleteResource);
 
 export default resourceRoute;
