027d0dee0949ebf6c9641e1e49e8b9d0a3b8898b
morch20 <82046309+morch20@users.noreply.github.com>
2024-06-05 14:07:08 -0400

    Update resourceSchema.ts

--- src/api/resources/resourceSchema.ts
+++ src/api/resources/resourceSchema.ts
@@ -1,10 +1,19 @@
 import { z } from "zod";
 
-const resourceSchema = z.object({
-    name: z.string().trim().min(3, "Name is required"),
-    ipAddress: z.string().trim().ip(),
+export const resourceSchemaRequest = z.object({
+    name: z
+        .string()
+        .trim()
+        .min(2, "Name must be 2 or more characters long.")
+        .max(225),
+    ip: z.string().trim().ip(),
 });
 
-export type Resource = z.infer<typeof resourceSchema>;
+export const resourceSchemaResponse = resourceSchemaRequest.extend({
+    updatedAt: z.date(),
+    createdAt: z.date(),
+    id: z.number().int().positive(),
+});
 
-export default resourceSchema;
+export type ResourceResponse = z.infer<typeof resourceSchemaResponse>;
+export type ResourceRequest = z.infer<typeof resourceSchemaRequest>;
