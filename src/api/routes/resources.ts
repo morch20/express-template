36f62124e9bb43e4188812fc13f530c4a898ce10
morch20 <82046309+morch20@users.noreply.github.com>
2024-06-05 14:03:24 -0400

    Update resources.ts

--- src/api/routes/resources.ts
+++ src/api/routes/resources.ts
@@ -1,19 +1,30 @@
 import validate from "@/middlewares/validate";
 import { Router } from "express";
+import { paramsSchema, paginationSchema } from "@/lib/validations";
 import { resourceSchema, resourceController } from "../resources";
 
 const resourceRoute = Router();
 
 resourceRoute
     .route("/")
-    .post(validate(resourceSchema, "body"), resourceController.createResource)
-    .get(resourceController.getResources);
+    .post(
+        validate(resourceSchema, "body"),
+        resourceController.createResource
+    )
+    .get(validate(paginationSchema, "query"), resourceController.getResources);
 
 // eslint-disable-next-line drizzle/enforce-delete-with-where
 resourceRoute
     .route("/:id")
-    .get(resourceController.getResource)
-    .patch(validate(resourceSchema, "body"), resourceController.updateResource)
-    .delete(resourceController.deleteResource);
+    .get(validate(paramsSchema, "params"), resourceController.getResource)
+    .patch(
+        validate(paramsSchema, "params"),
+        validate(resourceSchema, "body"),
+        resourceController.updateResource
+    )
+    .delete(
+        validate(paramsSchema, "params"),
+        resourceController.deleteResource
+    );
 
 export default resourceRoute;
