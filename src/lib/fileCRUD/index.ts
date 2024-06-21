9d1dff6cbf46a8c48e6d7d4c74a3b7d75510c929
morch20 <82046309+morch20@users.noreply.github.com>
2024-06-21 09:31:35 -0400

    Fixed logic errors

--- src/lib/fileCRUD/index.ts
+++ src/lib/fileCRUD/index.ts
@@ -1,8 +1,7 @@
 import * as fs from "fs-extra";
 import * as path from "node:path";
 import * as lockfile from "proper-lockfile";
-import httpStatus from "http-status";
-import { AppError } from "../errors";
+import { logger } from "../logger";
 
 export default class FileCRUD {
     /**
@@ -27,80 +26,68 @@ export default class FileCRUD {
     /**
      * Executes a callback function with a lock on the specified file.
      * Ensures that file operations are performed safely without race conditions.
-     * @param filePath - The path of the file to lock.
+     * @param filePath - The path of the file to lock. The file must exist.
      * @param callback - The async function to execute while the file is locked.
      * @param errorMessage - The error message to throw if the callback fails.
      * @returns A promise that resolves with the result of the callback or void.
      */
     public static async withLock(
         filePath: string,
-        callback: () => Promise<void | string>,
-        errorMessage: string
-    ): Promise<void | string> {
+        callback: () => Promise<null | string>
+    ): Promise<null | string> {
         const release = await lockfile.lock(filePath);
         try {
             return await callback();
         } catch (error) {
-            if (error instanceof AppError) {
-                throw error;
-            }
-            throw new AppError(
-                errorMessage,
-                httpStatus.INTERNAL_SERVER_ERROR,
-                true
-            );
+            logger.error("Something went wrong locking file", error as object);
+            return null;
         } finally {
             await release();
         }
     }
 
     /**
-     * Creates a new file with the specified content.
-     * Ensures the file does not already exist.
+     * Creates a new file with the specified content, replacing the file if it already exists.
      * @param fileName - The name of the file to create.
      * @param content - The content to write to the file.
-     * @throws AppError if the file already exists or if the operation fails.
+     * @returns A string "success" if file created successfully or null otherwise.
      */
-    async createFile(fileName: string, content: string): Promise<void> {
+    async createFile(
+        fileName: string,
+        content: string
+    ): Promise<null | string> {
         const filePath = this.getFilePath(fileName);
 
-        await FileCRUD.withLock(
-            filePath,
-            async () => {
-                if (await fs.pathExists(filePath)) {
-                    throw new AppError(
-                        `File ${fileName} already exists`,
-                        httpStatus.BAD_REQUEST
-                    );
-                }
-                await fs.writeFile(filePath, content);
-            },
-            `Failed to create file: ${fileName}`
-        );
+        // Ensure the file exists before attempting to create the file
+        // Otherwise the file lock will throw an error since there is not file to lock 🤦
+        await fs.ensureFile(filePath);
+
+        const response = await FileCRUD.withLock(filePath, async () => {
+            await fs.writeFile(filePath, content);
+            return "success";
+        });
+
+        return response;
     }
 
     /**
      * Reads the content of the specified file.
      * @param fileName - The name of the file to read.
-     * @returns A promise that resolves with the file content as a string.
-     * @throws AppError if the file does not exist or if the operation fails.
+     * @returns A promise that resolves with the file content as a string or null if file doesn't exit.
      */
-    async readFile(fileName: string): Promise<string> {
+    async readFile(fileName: string): Promise<null | string> {
         const filePath = this.getFilePath(fileName);
 
-        return (await FileCRUD.withLock(
-            filePath,
-            async () => {
-                if (!(await fs.pathExists(filePath))) {
-                    throw new AppError(
-                        `File ${fileName} does not exist`,
-                        httpStatus.BAD_REQUEST
-                    );
-                }
-                return fs.readFile(filePath, "utf8");
-            },
-            `Failed to read file: ${fileName}`
-        )) as string;
+        // Return null if file does not exit
+        // Otherwise the file lock will throw an error since there is not file to lock 🤦
+        if (!(await fs.pathExists(filePath))) return null;
+
+        const response = await FileCRUD.withLock(filePath, async () => {
+            const content = await fs.readFile(filePath, "utf8");
+            return content;
+        });
+
+        return response;
     }
 
     /**
@@ -108,45 +95,44 @@ export default class FileCRUD {
      * Ensures the file exists before updating.
      * @param fileName - The name of the file to update.
      * @param content - The new content to write to the file.
-     * @throws AppError if the file does not exist or if the operation fails.
+     * @returns A string "success" if file updated successfully or null otherwise.
      */
-    async updateFile(fileName: string, content: string): Promise<void> {
+    async updateFile(
+        fileName: string,
+        content: string
+    ): Promise<null | string> {
         const filePath = this.getFilePath(fileName);
-        await FileCRUD.withLock(
-            filePath,
-            async () => {
-                if (!(await fs.pathExists(filePath))) {
-                    throw new AppError(
-                        `File ${fileName} does not exist`,
-                        httpStatus.BAD_REQUEST
-                    );
-                }
-                await fs.writeFile(filePath, content);
-            },
-            `Failed to updated file: ${fileName}`
-        );
+
+        // Return null if file does not exit
+        // Otherwise the file lock will throw an error since there is not file to lock 🤦
+        if (!(await fs.pathExists(filePath))) return null;
+
+        const response = await FileCRUD.withLock(filePath, async () => {
+            await fs.writeFile(filePath, content);
+            return "success";
+        });
+
+        return response;
     }
 
     /**
      * Deletes the specified file.
      * Ensures the file exists before deleting.
      * @param fileName - The name of the file to delete.
-     * @throws AppError if the file does not exist or if the operation fails.
+     * @returns A string "success" if file updated successfully or null otherwise.
      */
-    async deleteFile(fileName: string): Promise<void> {
+    async deleteFile(fileName: string): Promise<null | string> {
         const filePath = this.getFilePath(fileName);
-        await FileCRUD.withLock(
-            filePath,
-            async () => {
-                if (!(await fs.pathExists(filePath))) {
-                    throw new AppError(
-                        `File ${fileName} does not exist`,
-                        httpStatus.BAD_REQUEST
-                    );
-                }
-                await fs.remove(filePath);
-            },
-            `Failed to delete file: ${fileName}`
-        );
+
+        // Return null if file does not exit
+        // Otherwise the file lock will throw an error since there is not file to lock 🤦
+        if (!(await fs.pathExists(filePath))) return null;
+
+        const response = await FileCRUD.withLock(filePath, async () => {
+            await fs.remove(filePath);
+            return "success";
+        });
+
+        return response;
     }
 }
