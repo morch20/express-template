80189e3738b77a66a542130aa882df1b8359e9bd
Joseph Maria <82046309+morch20@users.noreply.github.com>
2024-07-15 10:21:55 -0400

    Update schema.ts

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,25 @@
-import { pgTable, serial, varchar } from "drizzle-orm/pg-core";
+import {
+    pgTable,
+    serial,
+    varchar,
+    timestamp,
+    index,
+} from "drizzle-orm/pg-core";
 
 // eslint-disable-next-line import/prefer-default-export
-export const resource = pgTable("resource", {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 255 }).notNull(),
-    ip: varchar("ip", { length: 225 }).notNull(),
-});
+export const resource = pgTable(
+    "resource",
+    {
+        id: serial("id").primaryKey(),
+        name: varchar("name", { length: 255 }).notNull().unique(),
+        ip: varchar("ip", { length: 225 }).notNull(),
+        createdAt: timestamp("createdAt").notNull().defaultNow(),
+        updatedAt: timestamp("updatedAt")
+            .notNull()
+            .defaultNow()
+            .$onUpdate(() => new Date()),
+    },
+    (table) => ({
+        nameIndex: index("nameIndex").on(table.name),
+    })
+);
