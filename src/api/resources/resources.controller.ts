8347d46b02c270581e2280e764554a0634f666b5
Joseph Maria <82046309+morch20@users.noreply.github.com>
2024-07-15 10:09:55 -0400

    Update resources.controller.ts

--- src/api/resources/resources.controller.ts
+++ src/api/resources/resources.controller.ts
@@ -1,45 +1,70 @@
 import { AppError } from "@/lib/errors";
-import { catchAsync } from "@/lib/utils/functionts";
+import { catchAsync } from "@/lib/utils/functions";
 import httpStatus from "http-status";
 import resourceService from "./resource.services";
 
 const createResource = catchAsync(async (req, res) => {
-    const user = await resourceService.createResource(req.body);
-    res.status(httpStatus.CREATED).send(user);
+    // The validation middleware makes sure that the request body is a valid ResourceSchemaRequest
+    const result = await resourceService.createResource(req.body);
+
+    if (!result) {
+        throw new AppError(
+            "Error creating resource",
+            httpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    res.status(httpStatus.CREATED).json(result);
 });
 
 const getResources = catchAsync(async (req, res) => {
-    // TODO: validate filters and pagination and throw error
-    const filter = {};
-    const options = {};
-    const result = await resourceService.getResources(filter, options);
-    res.status(httpStatus.OK).send(result);
+    // The validation middleware makes sure "page" and "amount" are valid strings
+    // The default values are page = 1, amount = 10. Located in @/lib/validations
+    const page = Number(req.query.page);
+    const amount = Number(req.query.amount);
+
+    // The validation is handled in the middleware as well. Look at the resource route!
+    const name = req.query.name as string;
+
+    const result = await resourceService.getResources(page, amount, name);
+    res.status(httpStatus.OK).json(result);
 });
 
 const getResource = catchAsync(async (req, res) => {
-    // TODO: validate userId and throw error
-    const userId = Number(req.params.userId) || 0;
+    // The validation middleware makes sure "id" is a valid number
+    const id = Number(req.params.id);
 
-    const user = await resourceService.getResourceById(userId);
-    if (!user) {
-        throw new AppError("User not found", httpStatus.NOT_FOUND);
+    const resource = await resourceService.getResourceById(id);
+    if (!resource) {
+        throw new AppError("Resource not found", httpStatus.NOT_FOUND);
     }
-    res.status(httpStatus.OK).send(user);
+    res.status(httpStatus.OK).json(resource);
 });
 
 const updateResource = catchAsync(async (req, res) => {
-    // TODO: validate userId and throw error
-    const userId = Number(req.params.userId) || 0;
+    // The validation middleware makes sure "id" is a valid number
+    const id = Number(req.params.id);
 
-    const user = await resourceService.updateResourceById(userId, req.body);
-    res.status(httpStatus.OK).send(user);
+    const updatedResource = await resourceService.updateResourceById(
+        id,
+        req.body
+    );
+
+    if (!updatedResource) {
+        throw new AppError(
+            "Error updating resource",
+            httpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
+
+    res.status(httpStatus.OK).json(updatedResource);
 });
 
 const deleteResource = catchAsync(async (req, res) => {
-    // TODO: validate userId and throw error
-    const userId = Number(req.params.userId) || 0;
+    // The validation middleware makes sure "id" is a valid number
+    const id = Number(req.params.id);
 
-    await resourceService.deleteResourceById(userId);
+    await resourceService.deleteResourceById(id);
     res.status(httpStatus.NO_CONTENT).send();
 });
 
